8822f61eb4ff7c8230c9e0419e14de6f1e0ee318
Uladzislau Patseyeu <95624971+Gegcbr9319@users.noreply.github.com>
2022-02-03 17:45:28 +0300

    Add files via upload
    add all task

--- src/index.js
+++ src/index.js
@@ -14,34 +14,26 @@ module.exports = function check(str, bracketsConfig) {
   }
   //console.log(OPEN_BRACKETS, BRACKETS_PAIR);
   let stack = [];
-  let stack1 = [];
+ 
 
   for (let i = 0; i < str.length; i++) {
     let currentSymbol = str[i];
     
-    if (OPEN_BRACKETS.includes(currentSymbol) && BRACKETS_PAIR[currentSymbol] != currentSymbol)  {
+    if (OPEN_BRACKETS.includes(currentSymbol) && BRACKETS_PAIR[currentSymbol] === undefined)  {
      
       stack.push(currentSymbol);
-    } else if (BRACKETS_PAIR[currentSymbol] == currentSymbol && !stack1.includes(currentSymbol))  {
-stack1.push(currentSymbol);
+    } else if (BRACKETS_PAIR[currentSymbol] === currentSymbol && !stack.includes(currentSymbol) && OPEN_BRACKETS.includes(currentSymbol))  {
+stack.push(currentSymbol);
     } else 
     {
-      if (stack.length === 0 && stack1.length === 0)  {
+      if (stack.length === 0)  {
         return false;
       }
 
-      let topElement;
-      if (BRACKETS_PAIR[currentSymbol] === currentSymbol){
-        topElement = stack1[stack1.length - 1];
-      } else
-      topElement = stack[stack.length - 1]; 
-     // console.log(topElement, stack, stack1);
-
-      if (BRACKETS_PAIR[topElement] === topElement) {
-        stack1.pop();
-      } else if(BRACKETS_PAIR[currentSymbol] === topElement) {
+      let topElement = stack[stack.length - 1]; ;
+      if (BRACKETS_PAIR[currentSymbol] === topElement){
         stack.pop();
-      } else
+           } else
       {
         return false;
       }
